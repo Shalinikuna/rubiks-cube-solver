c0db17eab1e45e0ea8dc66c33d71136ddfdc3d9d
kunashalini <152308775+kunashalini@users.noreply.github.com>
2026-02-19 21:49:09 +0530

    Added kociemba to requirements

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import kociemba
 
 app = FastAPI()
 
+# Allow frontend access
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -11,52 +14,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class CubeInput(BaseModel):
+    cube_string: str
+
 @app.get("/")
 def home():
-    return {"message": "Rubik's Cube Scanner API is LIVE 🚀"}
+    return {"message": "Rubik's Cube Solver API is LIVE 🚀"}
 
-
-# Store faces temporarily
-cube_faces = []
-
-
-@app.post("/scan-face")
-def scan_face():
-    global cube_faces
-
-    # Dummy detected face (replace later with real detection)
-    detected_face = [
-        "Red", "Blue", "Green",
-        "White", "Yellow", "Orange",
-        "Red", "Blue", "Green"
-    ]
-
-    cube_faces.append(detected_face)
-
-    return {
-        "face_number": len(cube_faces),
-        "face_colors": detected_face
-    }
-
-
-@app.get("/solve")
-def solve_cube():
-    # Dummy algorithm
-    moves = ["R", "U", "R'", "U'"]
-
-    readable_steps = []
-
-    for move in moves:
-        if move == "R":
-            readable_steps.append("Rotate Right face clockwise")
-        elif move == "R'":
-            readable_steps.append("Rotate Right face anti-clockwise")
-        elif move == "U":
-            readable_steps.append("Rotate Upper face clockwise")
-        elif move == "U'":
-            readable_steps.append("Rotate Upper face anti-clockwise")
-
-    return {
-        "faces_scanned": len(cube_faces),
-        "steps": readable_steps
-    }
+@app.post("/solve")
+def solve_cube(data: CubeInput):
+    try:
+        solution = kociemba.solve(data.cube_string)
+        steps = solution.split(" ")
+        return {
+            "solution_steps": steps
+        }
+    except Exception as e:
+        return {"error": "Invalid cube configuration"}
